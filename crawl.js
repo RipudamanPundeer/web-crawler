a3f93ec53f08ecac64c65f90b5555faec76fac1a
RipudamanPundeer <pundeerripudaman@gmail.com>
2025-09-17 15:09:07 +0530

    added recursive crawl function

--- crawl.js
+++ crawl.js
@@ -1,5 +1,46 @@
 const { JSDOM } = require("jsdom")
 
+async function crawlPage(baseURL, currentURL, pages){    
+    const baseURLObj = new URL(baseURL)
+    const currentURLObj = new URL(currentURL)
+    if(baseURLObj.hostname !== currentURLObj.hostname){
+        return pages
+    }
+
+    const normalizedCurrentURL = normalizeURL(currentURL)
+    if(pages[normalizedCurrentURL] > 0){
+        pages[normalizedCurrentURL]++
+        return pages
+    }
+    pages[normalizedCurrentURL] = 1
+    
+    console.log(`Crawling ${currentURL}`)
+
+    try{
+        const resp = await fetch(currentURL)
+        if(resp.status > 399){
+            console.log(`error in fetch with status code: ${resp.status} on page: ${currentURL}`)
+            return pages
+        }
+        const contentType = resp.headers.get("content-type")
+        if(!contentType.includes("text/html")){
+            console.log(`non html response, content type: ${contentType} on page: ${currentURL}`)
+            return pages
+        }
+        const html = await resp.text()
+        nextURLs = getURLsFromHTML(html, baseURL)
+
+        for(const nextURL of nextURLs){
+            pages = await crawlPage(baseURL, nextURL, pages)
+        }
+        return pages
+    }
+    catch(err){
+        console.log(`error in fetch: ${err.message} on page: ${currentURL}`)
+    }
+}
+
+
 function getURLsFromHTML(html,baseURL){
     const urls = []
     const dom = new JSDOM(html)
@@ -37,4 +78,4 @@ function normalizeURL(urlString){
     return hostPath
 }
 
-module.exports = { normalizeURL, getURLsFromHTML }
+module.exports = { normalizeURL, getURLsFromHTML, crawlPage }
--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("Hello, World!");
+const {crawlPage} = require('./crawl.js')
+
+async function main(){
+    if(process.argv.length != 3){
+        console.log("Usage: npm start <URL>")
+        return
+    }
+    const baseURL = process.argv[2]
+    console.log(`Starting crawl of ${baseURL}`)
+    const pages = await crawlPage(baseURL, baseURL, {})
+    console.log('\n\n\n')
+    for(const [page, count] of Object.entries(pages)){
+        console.log(`${page}: ${count} pages`)
+    }
+}
+main()
